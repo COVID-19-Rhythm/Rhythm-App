a71d7b9d69761fa52f7ca18cd67c5b7448cf1c97
Andreas Ink <andreasink17@gmail.com>
2021-08-31 00:26:43 -0400

    Improved loading speeds and created two separate functions for risk scoring

--- CovidHRV/Data/Health/Health.swift
+++ CovidHRV/Data/Health/Health.swift
@@ -315,7 +315,101 @@ class Health: ObservableObject {
 //            //}
 //            return (self.risk, codableRisk)
 //        }
+    
     func getRiskScore(bedTime: Int, wakeUpTime: Int, data: [HealthData]) -> (Risk, [CodableRisk]) {
+                //DispatchQueue.main.asyncAfter(deadline: .now() + 30.0) {
+                print("Calculating Risk...")
+                if !healthData.isEmpty {
+                var medianHeartrate = 0.0
+                    let url3 = self.getDocumentsDirectory().appendingPathComponent("risk.txt")
+                do {
+                    
+                    let input = try String(contentsOf: url3)
+                    
+                    
+                    let jsonData = Data(input.utf8)
+                    do {
+                        let decoder = JSONDecoder()
+                        
+                        do {
+                            let codableRisk = try decoder.decode([CodableRisk].self, from: jsonData)
+                            
+                           // medianHeartrate = codableRisk.map{Int($0.risk)}.median()
+                            
+                         
+                        } catch {
+                            print(error.localizedDescription)
+                        }
+                    }
+                } catch {
+                    
+                }
+                
+                let filteredToNight = healthData.filter {
+                    return $0.date.get(.hour) > bedTime && $0.date.get(.hour) < wakeUpTime
+                }
+                let filteredToHeartRate = filteredToNight.filter {
+                    return $0.title == HKQuantityTypeIdentifier.heartRate.rawValue
+                }
+                let filteredToSteps = filteredToNight.filter {
+                    return $0.title == HKQuantityTypeIdentifier.stepCount.rawValue
+                }
+                let filteredToMoreThanZeroSteps = filteredToSteps.filter {
+                    return $0.data != 0
+                }
+                var todayHeartRates = [Double]()
+                var heartRates = [Double]()
+                var averageHRPerNight = [Double]()
+                var dates = [Date]()
+                print(healthData.map{$0.date})
+                for day in 0...30 {
+                let filteredToDay = filteredToHeartRate.filter { data in
+                    return data.date.get(.day) == day && data.date.get(.month) == Date().get(.month)
+                }
+                    heartRates = []
+                    print(filteredToDay)
+               
+                    if !filteredToMoreThanZeroSteps.map({$0.date}).contains(filteredToDay.last?.date ?? Date()) {
+                        if !filteredToDay.isEmpty {
+                            if Date().get(.day) == day {
+                                todayHeartRates.append(filteredToDay.isEmpty ? filteredToDay.last?.data ?? 0.0 : average(numbers: filteredToDay.map{$0.data}))
+                            } else {
+                                heartRates.append(filteredToDay.isEmpty ? filteredToDay.last?.data ?? 0.0 : average(numbers: filteredToDay.map{$0.data}))
+                            }
+                        
+                            
+                    dates.append(filteredToDay.last?.date ?? Date())
+                        }
+                    
+                }
+                    if !heartRates.isEmpty {
+                        averageHRPerNight.append(average(numbers: heartRates))
+                    }
+                }
+               print("averageHRPerNight")
+               print(averageHRPerNight)
+                medianHeartrate = averageHRPerNight.median()
+                    let riskScore = self.average(numbers: todayHeartRates) > medianHeartrate + 3 ? 1 : 0
+                    let explanation =  riskScore == 1 ? [Explanation(image: .exclamationmarkCircle, explanation: "Your health data may indicate you have an illness"), Explanation(image: .heart, explanation: "Calculated from your average heartrate while asleep"),  Explanation(image: .app, explanation: "Alerts may be triggered from other factors than an illness, such as lack of sleep, intoxication, or intense exercise"), Explanation(image: .stethoscope, explanation: "This is not a medical diagnosis, it's an alert to consult with your doctor")] : [Explanation(image: .checkmark, explanation: "Your health data may indicate you do not have an illness"), Explanation(image: .chartPie, explanation: "Calculated from your average heartrate while asleep"), Explanation(image: .stethoscope, explanation: "This is not a medical diagnosis or lack thereof, you may still have an illness")]
+                let risk = Risk(id: UUID().uuidString, risk: CGFloat(riskScore), explanation: explanation)
+                #warning("Change to a highher value to prevent bad data (because of low amount of data)")
+                if averageHRPerNight.count > 0 {
+                withAnimation(.easeOut(duration: 1.3)) {
+                    
+                self.risk = risk
+                }
+                    self.codableRisk.append(CodableRisk(id: risk.id, date: dates.last ?? Date(), risk: risk.risk, explanation: [String]()))
+                } else {
+                    //#warning("If last night's heartrate is empty, then alert goes off incorrectly")
+                    self.risk = Risk(id: "NoData", risk: CGFloat(21), explanation: [Explanation(image: .exclamationmarkCircle, explanation: "Wear your Apple Watch as you sleep to see your data")])
+                }
+                    //print(self.codableRisk)
+                }
+               
+                //}
+                return (self.risk, codableRisk)
+            }
+    func getRiskScoreAll(bedTime: Int, wakeUpTime: Int, data: [HealthData]) -> (Risk, [CodableRisk]) {
             //DispatchQueue.main.asyncAfter(deadline: .now() + 30.0) {
             print("Calculating Risk...")
             if !healthData.isEmpty {
@@ -383,12 +477,12 @@ class Health: ObservableObject {
                     if !filteredToDay.isEmpty {
                       //  print(filteredToDay.last?.date.get(.day) == day)
                         #warning("Change back")
-                        if 25 == day && month == 4 {
-                            todayHeartRates.append(filteredToDay.isEmpty ? filteredToDay.last?.data ?? 0.0 : average(numbers: filteredToDay.map{$0.data}))
-                          
-                        } else {
+//                        if 25 == day && month == 4 {
+//                            todayHeartRates.append(filteredToDay.isEmpty ? filteredToDay.last?.data ?? 0.0 : average(numbers: filteredToDay.map{$0.data}))
+//
+//                        } else {
                             heartRates.append(filteredToDay.isEmpty ? filteredToDay.last?.data ?? 0.0 : average(numbers: filteredToDay.map{$0.data}))
-                        }
+                        //}
                     
                         
                 dates.append(filteredToDay.last?.date ?? Date())
@@ -404,11 +498,13 @@ class Health: ObservableObject {
             }
            print("averageHRPerNight")
            print(averageHRPerNight)
-                if !averageHRPerNight.isEmpty {
-            medianHeartrate = averageHRPerNight.median()
-                }
+              
                 for night in averageHRPerNight {
                     #warning("exclude the night I am calculating the risk for from the overall medianHeartrate to ensure accuracy")
+                    if !averageHRPerNight.isEmpty {
+                        
+                        medianHeartrate = averageHRPerNight.filter {$0 != night}.median()
+                    }
                 let riskScore = night > medianHeartrate + 3 ? 1 : 0
                 print(riskScore)
                 let explanation =  riskScore == 1 ? [Explanation(image: .exclamationmarkCircle, explanation: "Your health data may indicate you have an illness"), Explanation(image: .heart, explanation: "Calculated from your average heartrate while asleep"),  Explanation(image: .app, explanation: "Alerts may be triggered from other factors than an illness, such as lack of sleep, intoxication, or intense exercise"), Explanation(image: .stethoscope, explanation: "This is not a medical diagnosis, it's an alert to consult with your doctor")] : [Explanation(image: .checkmark, explanation: "Your health data may indicate you do not have an illness"), Explanation(image: .chartPie, explanation: "Calculated from your average heartrate while asleep"), Explanation(image: .stethoscope, explanation: "This is not a medical diagnosis or lack thereof, you may still have an illness")]
